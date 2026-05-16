c7cf3b5781d524437cf16eadb5f7f40fedad81c6
Vahn Kessler <vahnkessler@MacBook-Pro-3.local>
2026-05-16 16:43:38 -0500

    sliders displaying basic param values

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -32,29 +32,48 @@ void LookAndFeel::drawRotarySlider(juce::Graphics & g,
     g.setColour(Colour(255u, 154u, 1u));
     g.drawEllipse(bounds, 1.f);
     
-    // draw thumb "hand"
-    auto center = bounds.getCentre();
-    Path p;
-    // rectangle is ± 2px from center
-    Rectangle<float> r;
-    r.setLeft(center.getX() - 2);
-    r.setRight(center.getX() + 2);
-    r.setTop(bounds.getY());
-    r.setBottom(center.getY());
-    
-    p.addRectangle(r);
-    
-    jassert(rotaryStartAngle < rotaryEndAngle);
-    
-    // map slider's normalized value to a radian angle
-    auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
-    
-    // rotate rectangle to sliderAngRad using an affine transform
-    p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
-    
-    g.fillPath(p);
-    
+    if( auto* rswl = dynamic_cast<RotarySliderWithLabels*>(&slider))
+    {
+        // draw "hand"
+        auto center = bounds.getCentre();
+        Path p;
+        // rectangle is ± 2px from center
+        Rectangle<float> r;
+        r.setLeft(center.getX() - 2);
+        r.setRight(center.getX() + 2);
+        r.setTop(bounds.getY());
+        r.setBottom(center.getY() - rswl->getTextHeight() * 1.5); // need to subtract text height from center so it doesn't overlap
+        
+        p.addRoundedRectangle(r, 2.f);
+        
+        jassert(rotaryStartAngle < rotaryEndAngle);
+        
+        // map slider's normalized value to a radian angle
+        auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
+        
+        // rotate rectangle to sliderAngRad using an affine transform
+        p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+        
+        g.fillPath(p);
+        
+        // set text size and font
+        g.setFont(rswl->getTextHeight());
+        auto text = rswl->getDisplayString();
+        auto strWidth = juce::GlyphArrangement::getStringWidth(g.getCurrentFont(), text);
+        
+        r.setSize(strWidth + 4, rswl->getTextHeight() + 2);
+        r.setCentre(center);
+        
+        // black background
+        g.setColour(Colours::black);
+        g.fillRect(r);
+        
+        // render text
+        g.setColour(Colours::white);
+        g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
+    }
 }
+
 //==============================================================================
 void RotarySliderWithLabels::paint(juce::Graphics &g)
 {
@@ -89,7 +108,7 @@ juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
     auto bounds = getLocalBounds();
     
     auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
-    
+     
     size -= getTextHeight() * 2;
     juce::Rectangle<int> r;
     r.setSize(size, size);
@@ -97,6 +116,11 @@ juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
     r.setY(2);
     
     return r;
+}
+
+juce::String RotarySliderWithLabels::getDisplayString() const
+{
+    return juce::String(getValue());
 }
 
 //==============================================================================
